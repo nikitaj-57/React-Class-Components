92ed937c41d7671a0bbd5bcb57905fe865751ff8
nikitaj-57 <nikitajit2@gmail.com>
2023-05-18 08:17:10 +0530

    state practise

--- src/App.js
+++ src/App.js
@@ -1,78 +1,30 @@
 import React from "react";
 import "./styles.css";
 
-const userData = {
-  name: "Nikita Jit",
-  handle: "nikitajit3",
-  avatar:
-    "https://png.pngtree.com/png-clipart/20190516/original/pngtree-cute-girl-avatar-material-png-image_4023832.jpg",
-};
-
-const friends = [
-  { id: 1, name: "A" },
-  { id: 2, name: "B" },
-  { id: 3, name: "C" },
-  { id: 4, name: "D" },
-  { id: 5, name: "E" },
-  { id: 6, name: "F" },
-];
-
-class Name extends React.Component {
-  render() {
-    const { name } = this.props;
-    return <h4>{name}</h4>;
-  }
-}
-
-class Handle extends React.Component {
-  render() {
-    const { handle } = this.props;
-    return <p>@{handle}</p>;
-  }
-}
-
-class Avatar extends React.Component {
-  render() {
-    const { avatarImg } = this.props;
-    return <img className="badgeImg" src={avatarImg} alt="" />;
-  }
-}
-
-class Badge extends React.Component {
-  render() {
-    const { userData } = this.props;
-    return (
-      <div className="handle">
-        <Avatar avatarImg={userData.avatar} />
-        <div>
-          <Name name={userData.name} />
-          <Handle handle={userData.handle} />
-        </div>
-      </div>
-    );
-  }
-}
-
-class ListComponent extends React.Component {
-  render() {
-    const { friends } = this.props;
-    return (
-      <ul style={{ color: "white" }}>
-        {friends.map((friend) => (
-          <li key={friend.id}>{friend.name}</li>
-        ))}
-      </ul>
-    );
-  }
-}
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "Nikita",
+    };
+
+    this.updateName = this.updateName.bind(this);
+  }
+
+  updateName(newName) {
+    this.setState({
+      name: newName,
+    });
+  }
   render() {
     return (
-      <div>
-        <Badge userData={userData} />;
-        <ListComponent friends={friends} />
-      </div>
+      <React.Fragment>
+        <h1>Hello, {this.state.name}</h1>
+        <button onClick={() => this.updateName("Nikita New")}>
+          Change name
+        </button>
+      </React.Fragment>
     );
   }
 }
