021e08e617809d48f57cb044bac72756915d5982
nikitaj-57 <nikitajit2@gmail.com>
2023-05-18 07:51:56 +0530

    props using class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userData = {
+  name: "Nikita Jit",
+  handle: "nikitajit3",
+  avatar:
+    "https://png.pngtree.com/png-clipart/20190516/original/pngtree-cute-girl-avatar-material-png-image_4023832.jpg",
+};
+
+const friends = [
+  { id: 1, name: "A" },
+  { id: 2, name: "B" },
+  { id: 3, name: "C" },
+  { id: 4, name: "D" },
+  { id: 5, name: "E" },
+  { id: 6, name: "F" },
+];
+
+class Name extends React.Component {
+  render() {
+    const { name } = this.props;
+    return <h4>{name}</h4>;
+  }
 }
 
-export default App;
+class Handle extends React.Component {
+  render() {
+    const { handle } = this.props;
+    return <p>@{handle}</p>;
+  }
+}
+
+class Avatar extends React.Component {
+  render() {
+    const { avatarImg } = this.props;
+    return <img className="badgeImg" src={avatarImg} alt="" />;
+  }
+}
+
+class Badge extends React.Component {
+  render() {
+    const { userData } = this.props;
+    return (
+      <div className="handle">
+        <Avatar avatarImg={userData.avatar} />
+        <div>
+          <Name name={userData.name} />
+          <Handle handle={userData.handle} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class ListComponent extends React.Component {
+  render() {
+    const { friends } = this.props;
+    return (
+      <ul style={{ color: "white" }}>
+        {friends.map((friend) => (
+          <li key={friend.id}>{friend.name}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Badge userData={userData} />;
+        <ListComponent friends={friends} />
+      </div>
+    );
+  }
+}
